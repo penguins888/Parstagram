6d62e4e225daae7466f298092ec68a5782e5f2d5
penguins888 <lu.michelle.e@gmail.com>
2020-03-26 15:24:27 -0700

    Parstagram pt.2 commits

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Michelle Lu on 3/25/20.
+//  Copyright © 2020 meep. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
